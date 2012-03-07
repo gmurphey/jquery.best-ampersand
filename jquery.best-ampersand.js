cb37c02e3eaf8f40eb3b02321af922cf7a947e3e
Garrett Murphey <gmurphey@gmurphey.com>
2012-03-07 01:53:21 -0500

    Minifying.

--- jquery.best-ampersand.js
+++ jquery.best-ampersand.js
@@ -1,9 +1,9 @@
 (function ($) {
 	$.fn.bestAmpersand = function (options) {
-		var settings = $.extend({ class: 'ampersand' }, options);
+		var settings = $.extend({ 'class': 'ampersand' }, options);
 
 		$(this).each(function () {
-			$(this).html($(this).html().replace(' &amp; ', ' <span class="' + settings.class + '">&</span> '));
+			$(this).html($(this).html().replace(' &amp; ', ' <span class="' + settings['class'] + '">&</span> '));
 		});
 	}
 }) (jQuery);
