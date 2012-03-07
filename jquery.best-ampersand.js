87913894cd3b7a06465057beff537bcec734480d
Garrett Murphey <gmurphey@gmurphey.com>
2012-03-07 00:35:00 -0500

    Making this work for real.

--- jquery.best-ampersand.js
+++ jquery.best-ampersand.js
@@ -1,9 +1,9 @@
 (function ($) {
-	$.fn.bestAmpersand(function (options = {}) {
+	$.fn.bestAmpersand = function (options) {
 		var settings = $.extend({ class: 'ampersand' }, options);
 
 		$(this).each(function () {
-			$(this).text($(this).text().replace('&', '<span class="' + settings.class + '">&</span>'));
+			$(this).html($(this).html().replace('&amp;', '<span class="' + settings.class + '">&</span>'));
 		});
-	});
+	}
 }) (jQuery);
